b52f17a0daa42208542ddcba732af44dc14c3df1
Josh <you@example.com>
2022-06-05 11:27:26 +0800

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,17 @@
+const key = "pLGcjF9lCpsjOly9UJRo2FFPrpAyg6QJ";
+
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
